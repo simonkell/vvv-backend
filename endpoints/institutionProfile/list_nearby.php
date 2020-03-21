a18e313f4c7c1e23441803bb8232477cec41995c
Leon <kontakt@leon-heinrichs.de>
2020-03-21 20:32:49 +0100

    (fix) check for protocol type

--- endpoints/institutionProfile/list_nearby.php
+++ endpoints/institutionProfile/list_nearby.php
@@ -8,36 +8,24 @@ use tools\HttpError;
 include(".." . DIRECTORY_SEPARATOR .".." . DIRECTORY_SEPARATOR . "config.php");
 include(".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "autoload.php");
 
-const REQUIRED_FIELDS = ['name', 'street', 'house_number', 'postal_code', 'city', 'description', 'user_id'];
 $master = new MasterController();
 /* SETUP */
 
-// Be sure the user is logged in!
-if(!$master->isSessionValid()) {
-    $master->errorResponse(new HttpError(401, "Bitte melden Sie sich zuerst an."));
-    return;
-}
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Be sure the user is logged in!
+    if(!$master->isSessionValid()) {
+        $master->errorResponse(new HttpError(401, "Bitte melden Sie sich zuerst an."));
+        return;
+    }
 
-$dataContent = file_get_contents("php://input");
-if (!$dataContent) {
-    http_response_code(400);
-    return;
-}
+    $user = $master->userController->getUserById($_SESSION[SESSION_NAME_USERID]);
 
-$data = json_decode($dataContent);
-
-$validator = new Validator($data, REQUIRED_FIELDS);
-$validationErrors = $validator->validate();
-if (!empty($validationErrors)) {
-    $master->errorResponse($validationErrors);
-    return;
-}
-
-if (true) {
-    http_response_code(200);
-    //$master->returnObjectAsJson($master->user);
-    return;
-} else {
-    http_response_code(401);
-    return;
+    if (true) {
+        http_response_code(200);
+        //$master->returnObjectAsJson($master->user);
+        return;
+    } else {
+        http_response_code(401);
+        return;
+    }
 }
