fc5f0dc68e01a94a0b439eb3c62458ceed05d793
App-rentice <14206678+App-rentice@users.noreply.github.com>
2020-03-21 14:38:59 +0100

    Add missing semicolon...

--- tools/HelperFunctions.php
+++ tools/HelperFunctions.php
@@ -5,12 +5,12 @@ function calc_distance($coord1, $coord2){
     $dLat = deg2rad($coord2[0] - $coord1[0]);
     $dLong = deg2rad($coord2[1] - $coord1[1]);
     
-    $a = sin($dLat/2) * sin($dLat/2) + cos($lat1) * cos($lat2) * sin($dLong/2) * sin($dLong/2);
+    $a = pow(sin($dLat/2), 2) + cos($lat1) * cos($lat2) * pow(sin($dLong/2), 2);
     $c = 2 * atan2(sqrt($a), sqrt(1-$a));
     return $r * $c;
 }
 
 //0 => latitude, 1 => longitude
 $leipzig = array(51.3406321, 12.3747329);
-$essen = array(51.4582235, 7.0158171)
+$essen = array(51.4582235, 7.0158171);
 //echo calc_distance($leipzig, $essen)." Meter";
