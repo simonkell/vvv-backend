eb882f46ad5da8d3880fe84b1f934012abd8d886
Leon <kontakt@leon-heinrichs.de>
2020-03-21 21:22:04 +0100

    (fix) rename method

--- endpoints/institutionProfile/get_by_user_id.php
+++ endpoints/institutionProfile/get_by_user_id.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     // Try to update profile. Timestamp for update will be set inside update function
-    $institutionProfile = $master->institutionController->getInstitutionProfileByUserId($master->user);
+    $institutionProfile = $master->institutionController->getInstitutionProfileByUser($master->user);
     if (isset($institutionProfile)) {
         http_response_code(200);
         $master->returnObjectAsJson($institutionProfile);
